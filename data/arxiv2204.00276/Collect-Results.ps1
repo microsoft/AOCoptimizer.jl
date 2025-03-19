1732bed85f5d86e1b8f51bcaa74ce033df57168b
Christos Gkantsidis <gkantsidis@gmail.com>
2025-03-19 14:37:47 +0000

    feat(benchmarks): creating inputs graphs for arxiv2204.00276

---
+++ data/arxiv2204.00276/Collect-Results.ps1
@@ -0,0 +1,50 @@
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory = $false)]
+    [string[]]$InputFile = @(Get-ChildItem -Path $PSScriptRoot -Filter "*.sol.json" | Select-Object -ExpandProperty FullName),
+
+    [Parameter(Mandatory = $false)]
+    [string]$OutputFile = "results.csv"
+)
+
+$results = @()
+foreach ($file in $InputFile) {
+    $graph = [System.IO.Path]::GetFileNameWithoutExtension($file).Replace(".sol", "")
+    $graph_size = $graph.Split("-")[1]
+
+    Write-Verbose "Processing $graph (of size $graph_size)"
+    $json = Get-Content -Path $file -Raw | ConvertFrom-Json
+    $objective = $json.SolutionInfo.ObjVal
+    $bound = $json.SolutionInfo.ObjBound
+
+    if ($objective -eq $bound) {
+        $results += [PSCustomObject]@{
+            Graph = $graph;
+            Size = $graph_size;
+            Metric = "GraphCut";
+            Property = "MaxCut";
+            Value = $objective
+        }
+    } else {
+        $results += [PSCustomObject]@{
+            Graph = $graph;
+            Size = $graph_size;
+            Metric = "GraphCut";
+            Property = "Heuristic";
+            Value = $objective
+        }
+        $results += [PSCustomObject]@{
+            Graph = $graph;
+            Size = $graph_size;
+            Metric = "GraphCut";
+            Property = "UpperBound";
+            Value = $bound
+        }
+    }
+}
+
+if (Test-Path -Path $OutputFile -PathType Leaf) {
+    Write-Warning -Message "Output file already exists. Deleting $OutputFile."
+    Remove-Item -Path $OutputFile -Force
+}
+$results | Export-Csv -Path $OutputFile -NoTypeInformation -Force -NoClobber -UseQuotes AsNeeded
