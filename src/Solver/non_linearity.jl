9ad3ebb8adad4dae304f77e38a40b7e47e847a0d
Christos Gkantsidis <gkantsidis@gmail.com>
2025-06-05 14:56:46 +0100

    added help messages and minor refactorings to non_linearity methods

--- src/Solver/non_linearity.jl
+++ src/Solver/non_linearity.jl
@@ -4,8 +4,22 @@ non_linearity.jl
 Implements efficient methods to apply non-linearity functions
 to arrays.
 
+This file provides implementations that wrap the element-wise
+functions of non-linearity functions like `sign`, `tanh`, and
+`binary` to work on arrays. It provides reasonable implementations
+for CPU and GPU backends (using `KernelAbstractions.jl`).
+For backend-specific implementations (e.g., CUDA), the
+corresponding code is implemented in the `ext/CUDAExt/non_linearity.jl`
+file---a similar approach should be followed for other backends
+that require specific implementations.
 =#
 
+#=
+If CUDA is not available, we just return an empty block and
+do not generate any CUDA-specific code.
+The actual implementation of the CUDA-specific code is in
+the `ext/CUDAExt/non_linearity.jl` file.
+=#
 function _cuda_non_linearity_extension_code(::BackendExtension, fn_name, fn)
     return Expr(:block)  # default empty block
 end
@@ -64,11 +78,24 @@ macro make_non_linearity(
 
 end #macro
 
+#=
+Since we do not control the order of package loading,
+we need to delay creating the non-linearities until
+the end to make sure that specific implementations
+(e.g., for CUDA) are available.
+
+Hence, the approach is to create a list of all non-linearities
+and then create them much later. Unfortunately, the user
+must explicitly initialize the package to make them available.
+The user must not call `__register_non_linearities` directly,
+and, instead, should call `AOCoptimizer.init()`.
+=#
+
 const __NON_LINEARITIES = []
 const __NON_LINEARITIES_LOCK = Threads.SpinLock()
 const __NON_LINEARITY_MACROS_EXPANDED = Ref(false)
 
-function register_non_linearities()
+function __register_non_linearities()
     if __NON_LINEARITY_MACROS_EXPANDED[]
         return
     end
@@ -94,6 +121,28 @@ function register_non_linearities()
         unlock(__NON_LINEARITIES_LOCK)
     end
 end
+
+"""
+    _non_linearity_sign!(x::AbstractArray{T,N}) where {T<:Real,N}
+
+In-place application of the sign function element-wise to the array `x`.
+"""
+function _non_linearity_sign! end
+
+"""
+    _non_linearity_tanh!(x::AbstractArray{T,N}) where {T<:Real,N}
+
+In-place application of the sign function element-wise to the array `x`.
+"""
+function _non_linearity_tanh! end
+
+"""
+    _non_linearity_binary!(x::AbstractArray{T,N}) where {T<:Real,N}
+
+In-place application of the function `x -> (x>0.5)?1.0:0.0`
+element-wise to the array `x`.
+"""
+function _non_linearity_binary! end
 
 push!(__NON_LINEARITIES, ("_non_linearity_sign", sign))
 push!(__NON_LINEARITIES, ("_non_linearity_tanh", tanh))
--- src/init.jl
+++ src/init.jl
@@ -6,5 +6,5 @@ init.jl
 function init()
     @info "Initializing AOCoptimizer..."
 
-    Solver.register_non_linearities()
+    Solver.__register_non_linearities()
 end
