8ca2ee95a1bb940550a53c4fb90ba09c478f5d5d
Christos Gkantsidis <gkantsidis@gmail.com>
2025-06-09 11:47:07 +0100

    worksheet for measuring sampler performance

--- notebooks/benchmark-sampler.jl
+++ notebooks/benchmark-sampler.jl
@@ -27,6 +27,45 @@ graph = -graph;
 λ = eigmax(graph);
 graph /= λ;
 
+# ## Run in a CPU
+
+interactions = copy(graph);
+
+binaries = size(interactions)[1];
+gradient = similar(interactions, number_of_experiments);
+momentum = similar(interactions, number_of_experiments);
+annealing_orig = similar(interactions, number_of_experiments);
+
+rand!(gradient);
+rand!(momentum);
+
+iterations = 200;
+annealing_orig .= T(1);
+
+x = similar(interactions, n, number_of_experiments);
+y = similar(x);
+fields = similar(x);
+spins = similar(x);
+
+y .= T(0);
+fields .= T(0);
+spins .= T(0);
+
+annealing = copy(annealing_orig);
+delta = annealing / iterations;
+dt = T(0.1);
+
+# Default sampler
+@benchmark Solver._sampler_internal!(interactions, nothing, binaries, gradient, momentum, x, y, fields, spins, annealing, delta, dt, iterations)
+
+# Binary sampler
+@benchmark Solver._sampler_binary_internal!(interactions, nothing, binaries, gradient, momentum, x, y, fields, spins, annealing, delta, dt, iterations)
+
+# QUMO sampler
+@benchmark Solver._sampler_qumo_internal!(interactions, nothing, binaries, gradient, momentum, x, y, fields, spins, annealing, delta, dt, iterations)
+
+# ## Run in an NVIDIA GPU (CUDA)
+
 interactions = cu(graph);
 
 binaries = size(interactions)[1];
@@ -53,7 +92,20 @@ annealing = copy(annealing_orig);
 delta = annealing / iterations;
 dt = T(0.1);
 
+# Default sampler
 CUDA.@bprofile begin
     Solver._sampler_internal!(interactions, nothing, binaries, gradient, momentum, x, y, fields, spins, annealing, delta, dt, iterations)
+    CUDA.synchronize()
+end
+
+# Binary sampler
+CUDA.@bprofile begin
+    Solver._sampler_binary_internal!(interactions, nothing, binaries, gradient, momentum, x, y, fields, spins, annealing, delta, dt, iterations)
+    CUDA.synchronize()
+end
+
+# QUMO sampler
+CUDA.@bprofile begin
+    Solver._sampler_qumo_internal!(interactions, nothing, binaries, gradient, momentum, x, y, fields, spins, annealing, delta, dt, iterations)
     CUDA.synchronize()
 end
