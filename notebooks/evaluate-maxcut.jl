26ca351bf6c843402f811110c35f654761179108
Christos Gkantsidis <gkantsidis@gmail.com>
2025-06-16 15:54:34 +0100

    test(quality): started working on testing on GSet graphs

---
+++ notebooks/evaluate-maxcut.jl
@@ -0,0 +1,33 @@
+#nb # %% A slide [markdown] {"slideshow": {"slide_type": "slide"}}
+
+# # Evaluate solver  performance on MaxCut problems.
+#
+# Input problems are from the G-Set library. For many of these problems,
+# the optimal solution is known; for the rest, there are best-known solutions.
+
+using Revise
+using CUDA
+using CSV
+using DataFrames
+using Dates
+using LinearAlgebra
+using SparseArrays
+using AOCoptimizer
+using AOCoptimizer.Solver
+
+CUDA.allowscalar(false)
+AOCoptimizer.init()
+
+T = Float32;
+
+input_path = joinpath(@__DIR__, "..", "data", "GSet");
+
+reference_file = joinpath(input_path, "reference.csv");
+baseline = CSV.read(reference_file, DataFrame);
+
+filename = "G1";
+input_file = joinpath(input_path, filename);
+graph = AOCoptimizer.FileFormats.read_graph_matrix(input_file);
+
+engine = AOCoptimizer.Solver.EngineCuda(0);
+sol = AOCoptimizer.Solver.solve(Float32, -graph, Second(100); engine=engine);
