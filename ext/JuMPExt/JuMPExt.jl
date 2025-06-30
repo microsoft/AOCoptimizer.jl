c0ba9bcaf3d5b4fbf1e629ba017d9e193552d236
Christos Gkantsidis <gkantsidis@gmail.com>
2025-06-30 15:32:48 +0100

    ported more code to connect to JuMP

--- ext/JuMPExt/JuMPExt.jl
+++ ext/JuMPExt/JuMPExt.jl
@@ -1,11 +1,20 @@
 #=
 JuMPExt.jl
 
+Extensions to allow the integration of the AOC optimizer with the JuMP framework.
+In reality, the extensions depend only on MathOptInterface, but will mostly be
+used through JuMP.
 
+The code implemented allows the user to use the AOC optimizer to solve "almost"-QUMO problems.
+This extension will transform the "almost"-QUMO problems to QUMO, and then invoke the
+AOCoptimizer solver to solve them. Below we implement very simple transformations.
+
+The code below is adapted by the work of Pedro
 =#
 
 module JuMPExt
 
+using LinearAlgebra
 import MathOptInterface as MOI
 import MathOptInterface: is_empty, empty!, optimize!
 import AOCoptimizer as AOC
@@ -22,6 +31,8 @@ const SAT{T}  = MOI.ScalarAffineTerm{T}
 const SAF{T}  = MOI.ScalarAffineFunction{T}
 const SQT{T}  = MOI.ScalarQuadraticTerm{T}
 const SQF{T}  = MOI.ScalarQuadraticFunction{T}
+
+const Engine = AOC.Solver.Engine
 
 include("variables.jl")
 include("wrapper.jl")
--- ext/JuMPExt/wrapper.jl
+++ ext/JuMPExt/wrapper.jl
@@ -96,3 +96,335 @@ end
 function Base.show(io::IO, ::Optimizer)
     return print(io, "AOC Optimizer")
 end
+
+include("attributes.jl")
+include("objective.jl")
+include("constraints.jl")
+include("solutions.jl")
+
+function MOI.supports(::Optimizer{T}, ::Type{X}, ::Type{Y}) where {T,X,Y}
+    @error("Do not support ($X, $Y)")
+
+    return false
+end
+
+function _copy_attributes!(optimizer, model)
+    # Copy attributes
+    for attr in MOI.get(model, MOI.ListOfModelAttributesSet())
+        if attr isa MOI.ObjectiveFunction
+            continue
+        end
+
+        if MOI.supports(optimizer, attr)
+            MOI.set(optimizer, attr, MOI.get(model, attr))
+        else
+            throw(MOI.UnsupportedAttribute(attr))
+        end
+    end
+
+    for attr in MOI.get(model, MOI.ListOfOptimizerAttributesSet())
+        if MOI.supports(optimizer, attr)
+            MOI.set(optimizer, attr, MOI.get(model, attr))
+        else
+            throw(MOI.UnsupportedAttribute(attr))
+        end
+    end
+
+    # Copy variable attributes
+    for attr in MOI.get(model, MOI.ListOfVariableAttributesSet())
+        if attr isa MOI.VariableName
+            continue
+        end
+
+        if MOI.supports(optimizer, attr, VI)
+            for vi in MOI.get(model, MOI.ListOfVariablesWithAttributeSet(attr))
+                MOI.set(optimizer, attr, vi, MOI.get(model, attr, vi))
+            end
+        else
+            throw(MOI.UnsupportedAttribute(attr))
+        end
+    end
+
+    # Copy constraint attributes
+    for (F, S) in MOI.get(model, MOI.ListOfConstraintTypesPresent())
+        for attr in MOI.get(model, MOI.ListOfConstraintAttributesSet{F,S}())
+            if MOI.supports(optimizer, attr, CI{F,S})
+                for ci in MOI.get(model, MOI.ListOfConstraintsWithAttributeSet{F,S}(attr))
+                    MOI.set(optimizer, attr, ci, MOI.get(model, attr, ci))
+                end
+            else
+                # throw(MOI.UnsupportedAttribute(attr))
+            end
+        end
+    end
+
+    return nothing
+end
+
+function MOI.copy_to(optimizer::Optimizer{T}, model::MOI.ModelLike) where {T}
+    @debug "Copying model for AOC/AIM optimizer"
+
+    index_map = MOIU.IndexMap()
+
+    _copy_attributes!(optimizer, model)
+
+    for (F, S) in MOI.get(model, MOI.ListOfConstraintTypesPresent())
+        if !MOI.supports_constraint(optimizer, F, S)
+            error("Unsupported constraint type: $F ∈ $S")
+        end
+    end
+
+    # Binary Variables
+    for ci in MOI.get(model, MOI.ListOfConstraintIndices{VI,MOI.ZeroOne}())
+        vi = MOI.get(model, MOI.ConstraintFunction(), ci)::VI
+
+        optimizer.variable_info[vi] = Variable{T}(:binary)
+
+        index_map[ci] = ci
+    end
+
+    # Intervals
+    for ci in MOI.get(model, MOI.ListOfConstraintIndices{VI,MOI.Interval{T}}())
+        vi = MOI.get(model, MOI.ConstraintFunction(), ci)::VI
+        si = MOI.get(model, MOI.ConstraintSet(), ci)::MOI.Interval{T}
+
+        @assert !haskey(optimizer.variable_info, vi) || optimizer.variable_info[vi].type === :continuous
+
+        optimizer.variable_info[vi] = Variable{T}(:continuous; lower = si.lower, upper = si.upper)
+
+        index_map[ci] = ci
+    end
+
+    # Lower Bounds
+    for ci in MOI.get(model, MOI.ListOfConstraintIndices{VI,GT{T}}())
+        vi = MOI.get(model, MOI.ConstraintFunction(), ci)::VI
+        si = MOI.get(model, MOI.ConstraintSet(), ci)::GT{T}
+
+        @assert !haskey(optimizer.variable_info, vi) || optimizer.variable_info[vi].type === :continuous || si.lower <= zero(T)
+
+        let v = if haskey(optimizer.variable_info, vi)
+                Variable{T}(:continuous; lower = si.lower, upper = optimizer.variable_info[vi].upper)
+            else
+                Variable{T}(:continuous; lower = si.lower)
+            end
+
+            if v.lower == v.upper # Fixed variable
+                optimizer.fixed[vi] = v.lower
+            end
+
+            optimizer.variable_info[vi] = v
+        end
+
+        index_map[ci] = ci
+    end
+
+    # Upper Bounds
+    for ci in MOI.get(model, MOI.ListOfConstraintIndices{VI,LT{T}}())
+        vi = MOI.get(model, MOI.ConstraintFunction(), ci)::VI
+        si = MOI.get(model, MOI.ConstraintSet(), ci)::LT{T}
+
+        @assert !haskey(optimizer.variable_info, vi) || optimizer.variable_info[vi].type === :continuous || si.upper >= one(T)
+
+        let v = if haskey(optimizer.variable_info, vi)
+                Variable{T}(:continuous; lower = optimizer.variable_info[vi].lower, upper = si.upper)
+            else
+                Variable{T}(:continuous; upper = si.upper)
+            end
+
+            if v.lower == v.upper # Fixed variable
+                optimizer.fixed[vi] = v.lower
+            end
+
+            optimizer.variable_info[vi] = v
+        end
+
+        index_map[ci] = ci
+    end
+
+    for vi in MOI.get(model, MOI.ListOfVariableIndices())
+        @assert haskey(optimizer.variable_info, vi) && is_bounded(optimizer.variable_info[vi]) "Unbounded variable $(vi)"
+
+        index_map[vi] = vi
+    end
+
+    for ci in MOI.get(model, MOI.ListOfConstraintIndices{VI,EQ{T}}())
+        vi = MOI.get(model, MOI.ConstraintFunction(), ci)::VI
+        si = MOI.get(model, MOI.ConstraintSet(), ci)::EQ{T}
+
+        if !haskey(optimizer.variable_info, vi)
+            optimizer.variable_info[vi] = Variable{T}(:continuous; lower = si.value, upper = si.value)
+        end
+
+        optimizer.fixed[vi] = si.value
+    end
+
+    # Fixed variables are not included in the variable list, so they won't get an index
+    variable_list = sort!(
+        filter(vi -> !haskey(optimizer.fixed, vi), collect(keys(optimizer.variable_info)));
+        by = (vi -> (optimizer.variable_info[vi].type === :continuous, vi.value))
+    )
+
+    optimizer.variable_map  = Dict{VI,Int}(vi => i for (i, vi) in enumerate(variable_list))
+    optimizer.continuous    = [optimizer.variable_info[vi].type === :continuous for vi in variable_list]
+
+    # Parse objective
+    let F = MOI.get(model, MOI.ObjectiveFunctionType())
+        f = MOI.get(model, MOI.ObjectiveFunction{F}())
+
+        Q, ℓ, c = _parse_objective(
+            f,
+            optimizer.variable_map,
+            optimizer.fixed,
+            optimizer.continuous
+        )
+
+        A, b = _scaling(optimizer.variable_info, optimizer.variable_map)
+
+        optimizer.quadratic = A' * Q * A
+        optimizer.linear    = vec(b' * Q * A + ℓ' * A)
+        optimizer.offset    = c + b' * Q * b + ℓ' * b
+    end
+
+    return index_map
+end
+
+function _parse_objective(vi::VI, vmap::Dict{VI,Int}, fixed::Dict{VI,T}, ::Vector{Bool}) where {T}
+    n = length(vmap)
+
+    Q = zeros(T, n, n)
+    ℓ = zeros(T, n)
+    c = zero(T)
+
+    if haskey(fixed, vi)
+        c += fixed[vi]
+    else
+        ℓ[vmap[vi]] = one(T)
+    end
+
+    return (Q, ℓ, c)
+end
+
+function _parse_objective(
+    f::SAF{T},
+    vmap::Dict{VI,Int},
+    fixed::Dict{VI,T},
+    ::Vector{Bool}
+) where {T}
+    n = length(vmap)
+
+    Q = zeros(T, n, n)
+    ℓ = zeros(T, n)
+    c = zero(T)
+
+    for term in f.terms
+        vi = term.variable
+        ci = term.coefficient
+
+        if haskey(fixed, vi)
+            c += ci * fixed[vi]
+        else
+            ℓ[vmap[vi]] = ci
+        end
+    end
+
+    return (Q, ℓ, c)
+end
+
+function _parse_objective(
+    f::SQF{T},
+    vmap::Dict{VI,Int},
+    fixed::Dict{VI,T},
+    continuous::Vector{Bool}
+) where {T}
+    n = length(vmap)
+
+    Q = zeros(T, n, n)
+    ℓ = zeros(T, n)
+    c = f.constant
+
+    for term in f.affine_terms
+        vi = term.variable
+        li = term.coefficient
+
+        if haskey(fixed, vi)
+            c += li * fixed[vi]
+        else
+            ℓ[vmap[vi]] += li
+        end
+    end
+
+    # Recall that the expression to optimize is -0.5 * x' * Q * x - ℓ' * x,
+    # i.e., there is a factor of 0.5 in the quadratic term.
+    for term in f.quadratic_terms
+        vi  = term.variable_1
+        vj  = term.variable_2
+        qij = term.coefficient
+
+        if haskey(fixed, vi) && haskey(fixed, vj)
+            c += qij * fixed[vi] * fixed[vj]
+        elseif haskey(fixed, vi)
+            ℓ[vmap[vj]] += qij * fixed[vi]
+        elseif haskey(fixed, vj)
+            ℓ[vmap[vi]] += qij * fixed[vj]
+        else
+            # neither is fixed
+            i = vmap[vi]
+            j = vmap[vj]
+
+            if continuous[i] || continuous[j]
+                if i == j
+                    Q[i, j] += T(2.0) * qij
+                else
+                    Q[i, j] += qij
+                    Q[j, i] += qij
+                end
+            elseif i == j
+                # both are binary, so we move the term to the linear part
+                ℓ[i] += qij
+            else
+                Q[i, j] += qij
+                Q[j, i] += qij
+            end
+        end
+    end
+
+    return (Q, ℓ, c)
+end
+
+function MOI.optimize!(optimizer::Optimizer{T}) where {T<:Real}
+    backend = MOI.get(optimizer, Backend())
+
+    seed       = something(MOI.get(optimizer, Seed()), trunc(Int, time()))
+    time_limit = trunc(Int, something(MOI.get(optimizer, MOI.TimeLimitSec()), 10.0))
+    work_dir   = MOI.get(optimizer, WorkDir())
+    num_type   = MOI.get(optimizer, NumericType())
+
+    quadratic = convert.(num_type, -optimizer.quadratic)
+    linear    = convert.(num_type, -optimizer.linear)
+
+    if optimizer.sense === MOI.MIN_SENSE
+        sense = AOC.MINIMIZATION
+    elseif optimizer.sense === MOI.MAX_SENSE
+        sense = AOC.MAXIMIZATION
+    else
+        error("Unknown sense $(optimizer.sense)")
+    end
+
+    @error "Solve here"
+
+    if !(T === num_type)
+        output = Dict(
+            "Objective"  => convert(T, output["Objective"]),
+            "Assignment" => convert.(T, output["Assignment"]),
+        )
+    else
+        output = Dict(
+            "Objective"  => output["Objective"],
+            "Assignment" => output["Assignment"],
+        )
+    end
+
+    optimizer.output = output
+
+    return nothing
+end
